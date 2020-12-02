ca7479a349a22431a6f2600e6b86753f198d49fc
Neal Schneier <nealschneier@ihsmarkit.com>
2020-12-02 18:41:38 -0500

    Part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -8,8 +8,34 @@ namespace Day2
         static void Main(string[] args)
         {
             var text = System.IO.File.ReadAllLines("input.txt");
+            Part1(text);
+            Part2(text);
+        }
+        private static void Part2(string[] text)
+        {
             var validCount = 0;
-            foreach(var line in text )
+            foreach (var line in text)
+            {
+                var items = line.Split(' ');
+                var firstNumber = int.Parse(items[0].Split('-')[0]);
+                var secondNumber = int.Parse(items[0].Split('-')[1]);
+                var character = items[1].ToCharArray()[0];
+                var characters = items[2].ToCharArray();
+                var firstChar = characters[firstNumber - 1];
+                var secondChar = characters[secondNumber - 1];
+                
+                if ((firstChar == character && secondChar != character) || (firstChar != character && secondChar == character))
+                {
+                    validCount += 1;
+                }
+            }
+            Console.WriteLine("Part 2 " + validCount);
+        }
+
+        private static void Part1(string[] text)
+        {
+            var validCount = 0;
+            foreach (var line in text)
             {
                 var items = line.Split(' ');
                 var firstNumber = int.Parse(items[0].Split('-')[0]);
@@ -21,7 +47,7 @@ namespace Day2
                     validCount += 1;
                 }
             }
-            Console.WriteLine(validCount);
+            Console.WriteLine("Part 1 " + validCount);
         }
     }
 }
