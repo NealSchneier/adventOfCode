e000420232fa4211cb21d2716d89140dd9c130e4
Neal Schneier <nealschneier@ihsmarkit.com>
2020-12-06 14:15:30 -0500

    part 1

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day3
 {
@@ -7,6 +8,42 @@ namespace Day3
         static void Main(string[] args)
         {
             var text = System.IO.File.ReadAllLines("input.txt");
+            var slope = new Slope { Down = 1, Right = 3};
+            var terrain = new Terrain();
+            terrain.Land = new List<bool>[text.Length];
+            var currentPosition = new Position();
+            var nextPosition = new Position{
+                x = slope.Right,
+                y = slope.Down
+            };
+            //parse the terrain
+            for (var i = 0; i < text.Length; i++)
+            {
+                var currentTerrain = new List<bool>();
+                foreach(char c in text[i])
+                {
+                    currentTerrain.Add(c == '#');
+                }
+                terrain.Land[i] = currentTerrain;
+            }
+            
+            //follow the tree down
+            for (var i = 0; i < terrain.Land.Length; i++)
+            {
+                var row = terrain.Land[i];
+                var xPosition = currentPosition.x;
+                if (currentPosition.x > row.Count)
+                {
+                    xPosition = xPosition % row.Count;
+                }
+                if (row[xPosition]) currentPosition.TreeCount += 1;
+                currentPosition.x = currentPosition.x +slope.Right;
+                currentPosition.y = currentPosition.y +slope.Down;
+
+            }
+            Console.WriteLine(currentPosition.x);
+            Console.WriteLine(currentPosition.y);
+            Console.WriteLine(currentPosition.TreeCount);
         }
     }
 }
