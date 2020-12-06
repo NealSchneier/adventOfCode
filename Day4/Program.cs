11bcb5c703351c47aee505587819d2d515c50681
Neal Schneier <nealschneier@ihsmarkit.com>
2020-12-06 18:23:26 -0500

    Day 4 part 1

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace Day4
@@ -9,52 +10,151 @@ namespace Day4
         {
             var text = System.IO.File.ReadAllText("Day4/input.txt");
             var passports = text.Split(Environment.NewLine + Environment.NewLine);
+            Part1(passports);
+            Part2(passports);
+        }
+        private static void Part2(string[] passports)
+        {
             var count = 0;
-            foreach(var passport in passports)
+            foreach (var passport in passports)
             {
                 var items = Regex.Split(passport, @"[\s \r\n]+");
                 var currentPassport = new Passport();
-                foreach(var item in items)
+                foreach (var item in items)
                 {
-                    if (item.Contains("byr:")) 
+                    if (item.Contains("byr:"))
                     {
-                        currentPassport.birthYear = int.Parse(item.Split(":")[1]);
+                        var year = int.Parse(item.Split(":")[1]);
+                        if(year >= 1920 && year <= 2002)
+                        {
+                            currentPassport.birthYear = year;
+                        }
                     }
-                    if (item.Contains("iyr:")) 
+                    if (item.Contains("iyr:"))
                     {
-                        currentPassport.issueYear = int.Parse(item.Split(":")[1]);
+                        var year = int.Parse(item.Split(":")[1]);
+                        if(year >= 2010 && year <= 2020)
+                        {
+                            currentPassport.issueYear = year;
+                        }
                     }
-                    if (item.Contains("eyr:")) 
+                    if (item.Contains("eyr:"))
                     {
-                        currentPassport.expirationYear = int.Parse(item.Split(":")[1]);
+                        var year = int.Parse(item.Split(":")[1]);
+                        if(year >= 2020 && year <= 2030)
+                        {
+                            currentPassport.expirationYear = year;
+                        }
                     }
-                    if (item.Contains("hgt:")) 
+                    if (item.Contains("hgt:"))
                     {
-                        currentPassport.height = item.Split(":")[1];
+                        var values = item.Split(":")[1];
+                        if (values.Contains("cm"))
+                        {
+                            var cmValue = int.Parse(values.Split("cm")[0]);
+                            if (cmValue >=150 && cmValue <= 193)
+                            {
+                                currentPassport.height = values;
+                            }
+                        } else if (values.Contains("in"))
+                        {
+                            var cmValue = int.Parse(values.Split("in")[0]);
+                            if (cmValue >=59 && cmValue <= 76)
+                            {
+                                currentPassport.height = values;
+                            }
+                        }
                     }
-                    if (item.Contains("hcl:")) 
+                    if (item.Contains("hcl:"))
                     {
-                        currentPassport.hairColor = item.Split(":")[1];
+                        var reg = new Regex("^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$");
+                        var match = reg.Match(item.Split(":")[1]);
+                        if (match.Success)
+                        {
+                            currentPassport.hairColor = item.Split(":")[1];
+                        }
                     }
-                    if (item.Contains("ecl:")) 
+                    if (item.Contains("ecl:"))
                     {
-                        currentPassport.eyeColor = item.Split(":")[1];
+                        var colors = new List<string> {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+                        if (colors.Contains(item.Split(":")[1]))
+                        {
+                            currentPassport.eyeColor = item.Split(":")[1];
+                        }
+                        
                     }
-                    if (item.Contains("pid:")) 
+                    if (item.Contains("pid:"))
                     {
-                        currentPassport.passportId = item.Split(":")[1];
+                        var value  = item.Split(":")[1];
+                        int intValue;
+                        var output = int.TryParse(value, out intValue);
+                        if (value.Length == 9 && output)
+                        {
+                            currentPassport.passportId = value;
+                        }
+                        
                     }
-                    if (item.Contains("cid:")) 
+                    if (item.Contains("cid:"))
                     {
                         currentPassport.countryId = int.Parse(item.Split(":")[1]);
                     }
                 }
-                if (currentPassport.IsValid())   
+                if (currentPassport.IsValid())
                 {
                     count += 1;
                 }
             }
-            Console.WriteLine(count);
+            Console.WriteLine("part 2 " + count);
+        }
+
+        private static void Part1(string[] passports)
+        {
+            var count = 0;
+            foreach (var passport in passports)
+            {
+                var items = Regex.Split(passport, @"[\s \r\n]+");
+                var currentPassport = new Passport();
+                foreach (var item in items)
+                {
+                    if (item.Contains("byr:"))
+                    {
+                        currentPassport.birthYear = int.Parse(item.Split(":")[1]);
+                    }
+                    if (item.Contains("iyr:"))
+                    {
+                        currentPassport.issueYear = int.Parse(item.Split(":")[1]);
+                    }
+                    if (item.Contains("eyr:"))
+                    {
+                        currentPassport.expirationYear = int.Parse(item.Split(":")[1]);
+                    }
+                    if (item.Contains("hgt:"))
+                    {
+                        currentPassport.height = item.Split(":")[1];
+                    }
+                    if (item.Contains("hcl:"))
+                    {
+                        currentPassport.hairColor = item.Split(":")[1];
+                    }
+                    if (item.Contains("ecl:"))
+                    {
+                        currentPassport.eyeColor = item.Split(":")[1];
+                    }
+                    if (item.Contains("pid:"))
+                    {
+                        currentPassport.passportId = item.Split(":")[1];
+                    }
+                    if (item.Contains("cid:"))
+                    {
+                        currentPassport.countryId = int.Parse(item.Split(":")[1]);
+                    }
+                }
+                if (currentPassport.IsValid())
+                {
+                    count += 1;
+                }
+            }
+            Console.WriteLine("part 1 " + count);
         }
     }
 }
