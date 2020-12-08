5d561d8c6e638398b7ef6c06b3204ee48b1410ed
Neal Schneier <neal.schneier@ihsmarkit.com>
2020-12-08 08:34:22 -0500

    add day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = System.IO.File.ReadAllText("Day6/input.txt");
+            var customsDocs = text.Split(Environment.NewLine + Environment.NewLine);
+            int count = 0;
+            foreach(var doc in customsDocs)
+            {
+                var array = doc.ToCharArray();
+                var list = new List<string>();
+                foreach(var c in array)
+                {
+                    list.Add(c.ToString());
+                }
+                count += list.Distinct().Count();
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
